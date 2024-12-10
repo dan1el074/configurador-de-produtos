7ddee8e4c1ebae238e2e3df99b2203231bb40b30
Daniel Vargas <daniel.rvargas10@gmail.com>
2024-12-10 15:41:22 -0300

    feat: add form in home controller

--- Views/pages/home.php
+++ Views/pages/home.php
@@ -1,26 +1,35 @@
-<h1>Home</h1>
-<p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel sagittis purus. Fusce et nisl in lacus porta
-    posuere. Cras convallis dolor ut eros tincidunt, ut fringilla lacus porta. Vestibulum rhoncus tortor ac felis
-    ornare, ut imperdiet ante faucibus. Integer sollicitudin vehicula pulvinar. Praesent porttitor pharetra leo ac
-    fermentum. Nullam sed augue lacus. Vestibulum aliquet metus sit amet libero sollicitudin, vitae aliquam sapien
-    mattis. Donec auctor ipsum non orci egestas sollicitudin. Vestibulum nec tristique tellus, quis facilisis ipsum. Sed
-    a egestas erat, nec suscipit velit. Duis pellentesque dignissim urna dictum molestie. Nullam quis lacinia lorem.
-    Fusce vitae faucibus ante. Nam at magna eu nisl consectetur rhoncus. Donec sollicitudin mattis nisl in consectetur.
-</p>
-<p>
-    Duis condimentum, lectus non gravida mollis, dolor urna fringilla erat, in gravida felis nulla vitae neque. Proin
-    condimentum mi at quam blandit convallis. Donec malesuada, neque id ultrices pellentesque, sapien augue cursus
-    nulla, ac fermentum nunc nibh at augue. Ut imperdiet ipsum quis tortor luctus ultricies. Curabitur ornare est et
-    ligula elementum rutrum. Mauris et dui est. Vestibulum vitae odio ut ante volutpat convallis id id libero. Sed
-    hendrerit fermentum ligula vel pharetra. Maecenas ullamcorper urna eu pharetra mollis. Vivamus ac iaculis arcu.
-    Vestibulum varius mauris at neque pulvinar faucibus. Nulla lacinia magna vitae placerat convallis. Nulla lacinia
-    libero at ante semper, interdum tempus elit sodales. Cras ornare euismod ullamcorper. Vivamus bibendum nec orci
-    commodo pretium. Quisque tempus dignissim lacus ut pretium.
-</p>
-<p>
-    Cras suscipit, turpis vitae scelerisque ultrices, nibh enim posuere justo, et porttitor erat orci eu sapien. Cras
-    non porttitor urna. Morbi ut est dictum, convallis ex tempor, elementum nibh. Cras interdum leo vel orci consequat
-    malesuada. Nulla facilisi. Etiam pellentesque ante at tincidunt pulvinar. Integer vulputate, elit non sagittis
-    fringilla, mi nibh tristique arcu, non sagittis purus nisi nec nunc.
-</p>
+<section class="productForm">
+    <h1>Gerador de nomenclatura</h1>
+
+    <div class="nav step1">
+        <div class="ball"></div>
+        <div class="row"></div>
+        <div class="ball"></div>
+        <div class="row"></div>
+        <div class="ball"></div>
+    </div>
+
+    <form action="" method="post">
+        <label for="order">Pedido:</label>
+        <input type="text" name="order" id="order">
+
+        <label for="product">Produto:</label>
+        <select name="product" id="product">
+            <?php foreach($atributes['produtos'] as $product) { ?>
+                <option value="<?= $product->getId() ?>"><?= "{$product->getName()} - {$product->getAbbreviation()}" ?></option>
+            <?php } ?>
+        </select>
+        
+        <label for="finish">Acabemento superficial:</label>
+        <select name="finish" id="finish">
+            <?php foreach($atributes['acabamentos'] as $finish) { ?>
+                <option value="<?= $finish->getId() ?>"><?= $finish->getName() . ($finish->getAbbreviation() ? " - " . $finish->getAbbreviation() : "")?></option>
+            <?php } ?>
+        </select>
+
+        <div class="button-content">
+            <button type="submit"><span>Proximo</span></button>
+        </div>
+    </form>
+
+</section>
