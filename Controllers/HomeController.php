4ac1b8632bcf36b108be2ea5956dd064ff740382
Daniel Vargas <daniel.rvargas10@gmail.com>
2024-12-10 15:43:40 -0300

    feat: add product and finish services

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -1,13 +1,23 @@
 <?php 
     namespace Controllers;
 
+use Services\AcabamentoService;
+use Services\ProdutoService;
+
     final class HomeController extends Controller{
+        private ProdutoService $produtoService;
+        private AcabamentoService $acabamentoService;
+
         public function __construct() {
+            $this->produtoService = new ProdutoService();
+            $this->acabamentoService = new AcabamentoService();
             $this->view = new \Views\MainView('home');
         }
+
         public function execute(): void {
-            $this->view->render(['titulo'=>'home']);
+            $produtoArray = $this->produtoService->findAll();
+            $acabamentoarray = $this->acabamentoService->findAll();
+            $this->view->render(['titulo'=>'home','produtos'=>$produtoArray,'acabamentos'=>$acabamentoarray]);
         }
     }
-
 ?>
